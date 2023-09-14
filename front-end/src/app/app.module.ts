9be77738a626604c93c98494ebe7793302e3a2ca
Rafael Santos Neves <r4fael.neves2004@gmail.com>
2023-09-14 03:48:59 -0300

    adicionando header

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/header/header.component';
+import { BannerComponent } from './home/banner/banner.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
